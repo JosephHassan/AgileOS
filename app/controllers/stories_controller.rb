7edeb475aa08953658369acf05a40f93e59b84d3
Joseph Hassan <konaman@gmail.com>
2013-12-20 22:01:48 -0800

    updated story form to accept a user assignment

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -69,6 +69,6 @@ class StoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def story_params
-      params.require(:story).permit(:title, :description, :status)
+      params.require(:story).permit(:title, :description, :status, :user_id)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+  
+  def full_name
+    return "#{first_name} #{last_name}"
+  end
+  
 end
