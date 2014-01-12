6e335f792e306439d3b8acb97ede3fd8eeca2c07
Joseph Hassan <konaman@gmail.com>
2014-01-12 00:08:17 -0800

    added scope to story model

--- app/models/story.rb
+++ app/models/story.rb
@@ -10,4 +10,11 @@ class Story < ActiveRecord::Base
   
   STATUS = ["New", "In Progress", "Ready for QA Testing", "Ready for PO Review", "Ready to Deploy", "Deployed"]
   FIBONACCI_SEQUENCE = [1, 2, 3, 5, 8, 13, 21, 34]
+
+  scope :new, -> {where(status: "New")}
+  scope :in_progress, -> {where(status: "In Progress")}
+  scope :ready_for_qa_testing, -> {where(status: "Ready for QA Testing")}
+  scope :ready_for_po_review, -> {where(status: "Ready for PO Review")}
+  scope :ready_to_deploy, -> {where(status: "Ready to Deploy")}
+  scope :open, -> {where(status: ["In Progress", "Ready for QA Testing", "Ready for PO Review", "Ready to Deploy"])}
 end
