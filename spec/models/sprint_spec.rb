58b39dcf80a1c566c0dac9c6df9ac979c93897f8
Joseph Hassan <konaman@gmail.com>
2014-01-24 22:07:07 -0800

    RSpec for sprint plus some ignore for IDE files

--- spec/models/sprint_spec.rb
+++ spec/models/sprint_spec.rb
@@ -25,18 +25,42 @@ describe Sprint do
       @new_sprint.should respond_to(:end_date)
     end
 
-    it 'has a status method that returns status based on various conditions' do
+    it 'has a status field that returns status based on various conditions' do
       @new_sprint.should respond_to(:status)
     end
 
-    it 'has a committed_number_of_stories field'
-    it 'has a completed_number_of_stories field'
-    it 'has an is_active? method'
+    it 'has a committed_number_of_stories field' do
+      #method should exist
+      @new_sprint.should respond_to(:committed_number_of_stories)
+
+      #TODO: method should return the value of stories committed to by the team
+
+    end
+
+    it 'has a completed_number_of_stories field' do
+      #method should exist
+      @new_sprint.should respond_to(:completed_number_of_stories)
+
+      #TODO: method should return the number of completed stories for this sprint by the team
+    end
+
+    it 'has an is_active? method' do
+      @new_sprint.should respond_to(:is_inactive?)
+    end
+
     it 'is_active? method returns true if today is within start and end date'
-    it 'has an is_past_due? method'
+
+    it 'has an is_past_due? method' do
+      @new_sprint.should respond_to(:is_past_due?)
+    end
+
     it 'is_past_due? method returns true if today is past the start date but the status has not shown a start'
 
-    it 'has an in_planning? method that returns true if there are no start or end dates'
+    it 'has an in_planning? method' do
+      @new_sprint.should respond_to(:in_planning?)
+    end
+
+    it 'in_planning? method returns true if there are no start or end dates'
 
   end
 
